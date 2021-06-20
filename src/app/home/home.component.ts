b9f2f8ceb75e7b26a2fdc3d89c447eb069a3b80b
Jose Sperandio <jose1234roberto@gmail.com>
2021-06-20 20:28:17 -0300

    pushToMaster

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../ofertas.service'
+import {Oferta} from '../shared/oferta.model'
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  //Provider imported by OfertasService from ofertas.service.ts made by me
+  providers: [ OfertasService ]
+})
+export class HomeComponent implements OnInit {
+  //declarando variavel service e criando a variavel como sendo um atributo do objeto
+
+
+  public ofertas!: Oferta []
+                                      //inferindo o tipo de acordo com o ts OfertasService
+  constructor(private OfertasService: OfertasService) { }
+
+  ngOnInit() {
+    //this acessando o metodo get.ofertas
+    this.ofertas = this.OfertasService.getOfertas()
+    console.log(this.ofertas)
+  }
+
+}
